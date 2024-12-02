443fc59945b68978e12c3776c962959fc53c93fc
s7xs94pnd <dastanovdanil0207@gmail.com>
2024-12-02 07:39:31 +0600

    Initial commit

---
+++ app/src/main/java/com/example/noty/ui/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.noty.ui.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.noty.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
